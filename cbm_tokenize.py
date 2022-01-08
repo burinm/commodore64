b2f4689ec703aff4ba5cc6d5e241d7bcd048294e
burin <21989631+burinm@users.noreply.github.com>
2022-01-08 15:19:33 -0700

    Ignore things in quotes and don't space when comparing/assigning strings

--- cbm_tokenize.py
+++ cbm_tokenize.py
@@ -17,11 +17,11 @@ import sys
 tokens = "end for next data input# input dim read let goto run if restore gosub return rem stop on wait load save verify def poke print# print cont list clr cmd sys open close get new tab to fn spc then not step \+ - \* / ^ and or > = < sgn int abs usr fre pos sqr rnd log exp cos sin tan atn peek len str\$ val asc chr\$ left\$ right\$ mid\$".split()
 
 # And I like to have no spaces in these keywords with () required:
-no_spaces = "asc atn chr\$ cos exp fre int left\$ len log mid\$ peek right\$ rnd sgn sin spc sqr str\$ tab tan usr val".split()  
+no_spaces = "asc atn chr\$ cos exp fre int left\$ len log mid\$ peek right\$ rnd sgn sin spc sqr str\$ tab tan usr val".split()
 
 # TODO - make command line option
 # Don't put spaces around operators (my preference)
-no_operators = "\+ - \* / ^ > = <".split() 
+no_operators = "\+ - \* / ^ > = <".split()
 
 tokens = set(tokens) - set(no_spaces) - set(no_operators)
 
@@ -30,14 +30,27 @@ for x in tokens:
     token_regex += x
     token_regex += "|"
 
-token_regex += (":)")  # also add ":" as token
+# Make quotes "stuff" it's own token
+#token_regex += ('".+?"|')  # Doesn't match ""
+token_regex += ('".*"|')
+# Add ":" as a token - and add closing ')' to regex
+token_regex += (":)")
 
-# TODO - leave things in quotes alone
 
 for line in sys.stdin:
-    tokenized = re.split(token_regex, line.strip()) 
+    tokenized = re.split(token_regex, line.strip())
     # print(tokenized)
     for tok in tokenized:
+        tok = tok.strip()
+        # hack to make a comparison operator followed by a
+        #  token have no spaces. This happens to just be:
+        #  <comparison>"string", all other combos are
+        #  nonsense.
+        if re.match('.*=$|.*<>$|.*<$|.*>$', tok):
+            # I like a="", not a= ""
+            print("{}".format(tok),end='')
+            continue
         if tok != '':
-            print("{} ".format(tok.strip()),end='')
+            print("{} ".format(tok),end='')
+            pass
     print("")
