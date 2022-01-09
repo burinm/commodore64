8043caa077d286d7b2f25c5e7588852e72b9ba27
burin <21989631+burinm@users.noreply.github.com>
2022-01-08 20:18:05 -0700

    proper comments for expand_d64.py

--- expand_d64.py
+++ expand_d64.py
@@ -4,6 +4,7 @@ import sys
 import os
 import re
 
+# Set programs here (vice includes c1541, petcat)
 C1541 = "/usr/local/bin/c1541"
 PETCAT = "/usr/local/bin/petcat"
 CBM_TOKENIZE = "/home/burin/src/commodore64/cbm_tokenize.py"
@@ -17,6 +18,7 @@ if (len(sys.argv) != 2):
     print("usage: expand_d64 <d64 image file>")
     sys.exit()
 
+# Read in d64 name from command line
 disk = sys.argv[1]
 
 print("\n[Using disk: {}]\n".format(disk))
@@ -25,29 +27,35 @@ if not os.path.exists(disk):
     print("{} not found".format(disk))
     sys.exit()
 
+# Get disk directory listing from c1541
 dirlist = os.popen(C1541 + " -attach " + disk + " -list").read()
 
 dirlist = dirlist.split('\n')
-header = dirlist.pop(0)
-
-# print header
+header = dirlist.pop(0)  # header summary is first line of dir listing
 print("[{}]".format(header))
 
-# header = [0 "X               " YY ZZ]
-# diskname = "X.YY"
+
+# Create a name for expansion directory based on header info
+#
+#   header = [0 "X               " YY ZZ]
+#   diskname = "X.YY"
+#
 diskbasename = header.split('"')[1].strip()
 diskdotname = header.split('"')[2].split()[0].strip()
 diskname = diskbasename + "." + diskdotname 
 diskname.strip()
 
-# Get all file names on disk
+# Gather all file names on disk
 files = {} 
 for f in dirlist:
+    # Skip summary line at end of dir listing
     if re.match(".*blocks free.*", f) or f == "":
         continue
-    # (), means keep regex match in split
+
+    # Split up entries of form [NN "filename" type]
+    #   (), means keep regex match in split
     unpacked = re.split('(".*")', f)
-    #print(unpacked)
+
     for index in range(len(unpacked)):
         unpacked[index] = unpacked[index].strip()
     
@@ -55,13 +63,14 @@ for f in dirlist:
         files[filename] = { 'size':size, 'type':ftype }
 
 
+# Summarize all files
 for k in files.keys():
    print("{:<20} {:<3} blocks {:<3}".format(k, files[k]['size'], files[k]['type']))
 
+# New disk directory name
 print("\n[{}]\n".format(diskname))
 
-
-
+# Approve new directory name
 while 1:
     print("use \"{}\" as disk name? ".format(diskname),end='')
     y_or_n = input()
@@ -74,8 +83,10 @@ while 1:
     else:
         break
     
+# Summarize directory name again, just because
 print("disk name \"{}\"".format(diskname))
 
+# Due dillegence, so we don't bork up filesystem
 if os.path.exists(diskname):
     print("\"{}\" file already exists!".format(diskname))
     print("exiting...")
@@ -86,7 +97,6 @@ if os.system("mkdir " + "\"" + diskname + "\"") != 0:
     print("exiting...")
     sys.exit()
 
-# Extract disk contents
 try:
     os.chdir(diskname)
 except FileNotFoundError:
@@ -94,10 +104,13 @@ except FileNotFoundError:
     print("exiting...")
     sys.exit()
 
-# At this point, script is inside new directory, no more safety checks
+# Extract disk contents
+#   At this point, script is inside new directory, no more safety checks
 newdir = os.popen("pwd").read()
 print("Expanding into:", newdir)
 
+
+# Extract .prg, .seq files into new directory
 EXTRACT_DISK_FILE = C1541 + " -attach ../" + disk + " -read "
 
 for k in files.keys():
@@ -111,6 +124,7 @@ for k in files.keys():
         print(command)
         os.system(command)
 
+# make "source" directory and populate from "binaries"
 if os.system("mkdir source") != 0:
     print("couldn't create \"source\" directory!")
     sys.exit()
@@ -123,5 +137,4 @@ for k in files.keys():
         os.system(command)
     if files[k]['type'] == "seq":
         print("skipping {}.seq, don't know how to convert".format(k))
-
 
